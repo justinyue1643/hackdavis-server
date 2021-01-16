91b185fc8ffc02770ca84cccda2261eb58506d5a
Justin Yue <justinyue1643@gmail.com>
2021-01-16 09:34:53 -0800

    add git ignore

--- index.js
+++ index.js
@@ -2,10 +2,21 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 
+const accountSid = process.env.ACCOUNT_SSID;
+const authToken = process.env.AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
 app.listen(process.env.PORT, () => {
     console.log("Listening on port " + process.env.PORT);
 });
 
-app.get("/", (req, res) => {
-    res.send("Bwoah");
+// 17135615426
+app.post("/", (req, res) => {
+    client.messages
+        .create({
+            body: "This is a third text",
+            from: "+17135615426",
+            to: "+17142092509"
+        })
+        .then(message => console.log(message))
 })
