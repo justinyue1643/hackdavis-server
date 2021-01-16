bd5ed64bd902ee279a04111fe3ace0eff5f63350
Justin Yue <justinyue1643@gmail.com>
2021-01-16 11:23:22 -0800

    print user message

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser');
+app.use(express.json());
 require('dotenv').config()
 
 const accountSid = process.env.ACCOUNT_SSID;
 const authToken = process.env.AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
-
-var phoneNumbers = ["+17142092509", "+17142347559"]
 
 app.listen(process.env.PORT, () => {
     console.log("Listening on port " + process.env.PORT);
@@ -30,22 +28,17 @@ app.get("/add-phone-number", (req, res) => {
 })
 
 app.post("/send-message", (req, res) => {
-    /*const message = req.body;
-    for (let i = 0; i < phoneNumbers.length; i++) {
-        client.messages
-            .create({
-                body: message,
-                from: "+17135615426",
-                to: phoneNumbers[i]
-            })
-            .then(message => console.log(message))
-    }
-    console.log(message);
-    res.send("Send Message Endpoint: Messages successfull sent!");*/
     const twiml = new MessagingResponse();
+    const userMessage = req.body.Body;
+
+    console.log(userMessage);
 
     twiml.message("Thank you for the thoughtful response. Your message will be posted shortly.");
 
     res.writeHead(200, {'Content-Type': 'text/xml'});
     res.end(twiml.toString());
 })
+
+app.post("/reply", (req, res) => {
+    res.send(req.body.message);
+});
