bca7fc6e48a9836b033abcad70094d07e24f372e
Justin Yue <justinyue1643@gmail.com>
2021-01-16 23:14:47 -0800

    adding phone update

--- routes/phoneRoute.js
+++ routes/phoneRoute.js
@@ -1,6 +1,10 @@
 const router = require("express").Router()
 const Phone = require("../mongo_schemas/phone.js");
 
+const accountSid = process.env.ACCOUNT_SSID;
+const authToken = process.env.AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
 router.route("/").get((req,res) => {
     Phone.find()
         .then((phoneNumbers) => res.json(phoneNumbers))
@@ -11,6 +15,14 @@ router.route("/add").post((req, res) => {
     const phoneNumber = new Phone({
         phone: req.body.phoneNumber
     });
+
+    client.messages
+        .create({
+            body: "Hello, you've just been added to therapME!",
+            from: "+17135615426",
+            to: req.body.phoneNumber,
+        })
+        .then(message => console.log(message))
 
     phoneNumber.save()
         .then(() => res.json("Successfully saved!"))
